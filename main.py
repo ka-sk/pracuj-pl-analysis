a482b46afd8f60db895175bb187c6caffa62837a
ka-sk <katarz.szczepaniak@gmail.com>
2026-08-06 18:08:15 +0000

    Seperate code in functions; Get list of urls to job offers

--- main.py
+++ main.py
@@ -1,17 +1,54 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+from time import sleep
 
 USER_AGENT_CONTACT = "katarz.szczepaniak@gmail.com"
 HEADER = {"User-Agent": f"PortfolioProjectResearch/0.1 (kontakt: {USER_AGENT_CONTACT})"}
+BASE_URL = "https://pracuj.pl/praca"
 
-# get page html code 
-response = requests.get("https://pracuj.pl/praca", headers=HEADER)
-json_data = BeautifulSoup(response.text, "html.parser").find("script", id="__NEXT_DATA__").string.encode()
-json_data = json.loads(json_data)
 
-data = json_data["props"]["pageProps"]["dehydratedState"]["queries"][0]["state"]["data"]["groupedOffers"]
+def get_search_output_json(url: str, header: dict) -> json: 
+    # get page html code 
+    try:
+        response = requests.get(url, headers=header)
+        json_data = BeautifulSoup(response.text, "html.parser").find("script", id="__NEXT_DATA__").string
+        json_data = json.loads(json_data)
+        # initial filtering out unnecessary tags
+        json_data = json_data["props"]["pageProps"]["dehydratedState"]["queries"][0]["state"]["data"]["groupedOffers"]
 
+    except Exception as e:
+        raise e
+    
+    return json_data
+
+
+def get_all_search_output(base_url: str, header: dict, limit_pages:int = None):
+    page_number = 1
+    whole_output = []
+    response = get_search_output_json(base_url, header)
+
+    while response != [] and page_number != limit_pages+1:
+        response = get_details_urls(response)
+        whole_output += response
+        page_number += 1
+        sleep(2)
+        response = get_search_output_json(f"{base_url}?pn={page_number}", header)
+        print(f"Loaded {page_number}. page")
+    return whole_output
+        
+
+
+def get_details_urls(offers_json: json)-> list:
+    urls_list = []
+    for element in offers_json:
+        urls_list.append(element["offers"][0]["offerAbsoluteUri"])
+    return urls_list
+
+with open("text.json", "w", encoding="utf-8") as f:
+    f.write('\n'.join(get_all_search_output(BASE_URL, HEADER, limit_pages=3)) + '\n')
+
+"""
 single_url = data[0]['offers'][0]['offerAbsoluteUri']
 
 # save json 
@@ -27,4 +64,4 @@ json_details = json.loads(json_details)
 json_details = json_details["props"]["pageProps"]["dehydratedState"]["queries"][0]["state"]["data"]
 
 with open("details.json", "w", encoding="utf-8") as f:
-    json.dump(json_details, f,ensure_ascii=False, indent=4)
+    json.dump(json_details, f,ensure_ascii=False, indent=4)"""
