c3c27a22c57753338e134ab297c507edb00b3b82
ka-sk <katarz.szczepaniak@gmail.com>
2026-08-09 12:10:08 +0200

    add detail scrapping; detail extraction in progress

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import json
 from time import sleep
+import os
+import pathlib
 
 USER_AGENT_CONTACT = "katarz.szczepaniak@gmail.com"
 HEADER = {"User-Agent": f"PortfolioProjectResearch/0.1 (kontakt: {USER_AGENT_CONTACT})"}
 BASE_URL = "https://pracuj.pl/praca"
+TEST_DATA_DIR = pathlib.Path("test_data").mkdir(parents=True, exist_ok=True)
 
 
 def get_search_output_json(url: str, header: dict) -> json: 
@@ -31,10 +34,10 @@ def get_all_search_output(base_url: str, header: dict, limit_pages:int = None):
     while response != [] and page_number != limit_pages+1:
         response = get_details_urls(response)
         whole_output += response
+        print(f"Loaded {page_number}. page")
         page_number += 1
         sleep(2)
         response = get_search_output_json(f"{base_url}?pn={page_number}", header)
-        print(f"Loaded {page_number}. page")
     return whole_output
         
 
@@ -45,8 +48,76 @@ def get_details_urls(offers_json: json)-> list:
         urls_list.append(element["offers"][0]["offerAbsoluteUri"])
     return urls_list
 
-with open("text.json", "w", encoding="utf-8") as f:
-    f.write('\n'.join(get_all_search_output(BASE_URL, HEADER, limit_pages=3)) + '\n')
+
+def get_details(url: str, header: dict)-> json:
+    details = requests.get(url, headers=header)
+
+    json_details = BeautifulSoup(details.text, "html.parser").find("script", id="__NEXT_DATA__").string
+    json_details = json.loads(json_details)
+
+    json_details = json_details["props"]["pageProps"]["dehydratedState"]["queries"][0]["state"]["data"]
+    return json_details
+
+
+def extract_details_from_json(offer_json: json):
+    '''
+    - offer id
+    - employerId
+    - jobOfferLanguage
+    - dateOfInitialPublicationUtc
+    - lastPublishedUtc
+    - expirationDateUtc
+    - isActive
+    - jobTitle
+    - isArchive
+    - isWithdrawn
+    - offerAbsoluteUrl
+    - displayEmployerName
+    - cateogries
+    - leadingCategory
+    - workplaces
+    - positionLevels
+    - entirelyRemoteWork
+    - workSchedules
+    - typesOfContracts with salary inside)
+    - salary 
+    - workModes
+    - "sectionType": "requirements"
+    - "sectionType": "offered"
+    - "code": "many-vacancies"
+    - "code": "immediate-employment"
+    - "code": "shift-work"
+    '''
+    pass
+
+def get_all_details(url_list, header, limit:int = None):
+    all_details = []
+    for url, idx in zip(url_list, range(len(url_list))):
+        if limit is not None and idx >= limit:
+            break
+
+        sleep(2)
+        json_details = get_details(url, header)
+        all_details.append(json_details)
+    print(f"Loaded {len(all_details)} offer details")
+    return all_details
+
+def save_to_db():
+    pass
+
+
+
+ulr_list = get_all_search_output(BASE_URL, HEADER, limit_pages=1)
+
+with open("test_data/text.txt", "w", encoding="utf-8") as f:
+    f.write('\n'.join(ulr_list) + '\n')
+
+details = get_all_details(ulr_list, header=HEADER, limit=3)
+
+for element, idx in zip(details, range(len(details))):
+    with open(f"test_data/details{idx}.json", "w", encoding="utf-8") as f:
+        json.dump(details,f, indent=4, ensure_ascii=False)
+
 
 """
 single_url = data[0]['offers'][0]['offerAbsoluteUri']
